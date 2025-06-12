621238e18cecf20f6c83b894a40b61dcc23d6b15
Matheus Anjos <matheus.anjos.01tec@gmail.com>
2025-06-11 23:21:28 -0300

    - Final 3.0 vacilei

--- locadoraprojeto/src/App.java
+++ locadoraprojeto/src/App.java
@@ -1,9 +1,9 @@
 //Matheus da Silva dos Anjos RGM: 38732513 - Karin Pereira da Silva RGM: 38815923 - Marcelo de Oliveira Cabral Sobrinho RGM: 38517680 - Pedro Guilherme Campos RGM: 38974223
 
-import view.ClienteView;
+import view.LocadoraView;
 
 public class App {
     public static void main(String[] args) {
-        new ClienteView().exibirMenu();
+        new LocadoraView().exibirMenu();
     }
 }
